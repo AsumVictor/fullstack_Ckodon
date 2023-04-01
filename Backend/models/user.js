b64c1f366a99348e9c553e631ac36febc8550eec
AsumVictor <Iamasum369@gmail.com>
2023-04-01 17:37:52 +0000

    BACKEND: connected to mongodb

---
+++ Backend/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  residence: {
+    type: String,
+    required: true,
+  },
+  role: [
+    {
+      type: String,
+      default: "undergraduate",
+    },
+  ],
+  school: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
--- Backend/server.js
+++ Backend/server.js
@@ -1,30 +1,46 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
-const path = require('path')
-const {logger } = require('./middleware/logger')
-const errorHandler = require('./middleware/errorHandler')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const corOptions = require('./config/corsOptions')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require("path");
+const { logger, logEvents } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
 
+connectDB();
 
-app.use(logger)
-app.use(cors(corOptions))
-app.use(express.json())
-app.use(cookieParser)
-app.use('/', express.static(path.join(__dirname, 'public')))
-app.use('/', require('./routes/root'))
-app.all('*', (req, res)=>{
-    res.status(404)
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname,'views', '404.html'))
-    } else if(req.accepts('json')){
-        res.json({message: '404 not found'})
-    }else{
-        res.type('txt').send('404 not found')
-    }
-}) 
+app.use(logger);
+app.use(cors(corOptions));
+app.use(express.json());
+app.use(cookieParser);
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", require("./routes/root"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 not found" });
+  } else {
+    res.type("txt").send("404 not found");
+  }
+});
 
-app.use(errorHandler)
-app.listen(PORT, ()=> console.log(`Server is running on ${PORT} `))
+app.use(errorHandler);
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to mongoDB");
+  app.listen(PORT, () => console.log(`Server is running on ${PORT} `));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoDBErrLog.log"
+  );
+});
