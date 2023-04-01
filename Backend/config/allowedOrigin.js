8d80253a47bf69e9887b70c49d3b618e15907404
AsumVictor <Iamasum369@gmail.com>
2023-04-01 12:21:07 +0000

    Backend: Added cors to allow origins, added middlewares:expressjson, loggers

---
+++ Backend/config/allowedOrigin.js
@@ -0,0 +1,6 @@
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://www.ckodon.com'
+]
+
+module.exports = allowedOrigins
--- Backend/server.js
+++ Backend/server.js
@@ -2,13 +2,23 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 const path = require('path')
+const {logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corOptions = require('./config/corsOptions')
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+
+app.use(logger)
+app.use(cors(corOptions))
+app.use(express.json())
+app.use(cookieParser)
+app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
 app.all('*', (req, res)=>{
     res.status(404)
     if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname,'/views', '/404.html'))
+        res.sendFile(path.join(__dirname,'views', '404.html'))
     } else if(req.accepts('json')){
         res.json({message: '404 not found'})
     }else{
@@ -16,4 +26,5 @@ app.all('*', (req, res)=>{
     }
 }) 
 
+app.use(errorHandler)
 app.listen(PORT, ()=> console.log(`Server is running on ${PORT} `))
