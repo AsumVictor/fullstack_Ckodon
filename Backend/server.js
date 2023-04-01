81da21255ca20c6a51c311971596bac6968eb298
AsumVictor <Iamasum369@gmail.com>
2023-04-01 09:07:01 +0000

    Backend and frontend setup

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const path = require('path')
+
+app.use('/', express.static(path.join(__dirname, '/public')))
+app.use('/', require('./routes/root'))
+app.all('*', (req, res)=>{
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname,'/views', '/404.html'))
+    } else if(req.accepts('json')){
+        res.json({message: '404 not found'})
+    }else{
+        res.type('txt').send('404 not found')
+    }
+}) 
+
+app.listen(PORT, ()=> console.log(`Server is running on ${PORT} `))
---
+++ userStory.md
@@ -0,0 +1,1 @@
+# Use cases and user stories
