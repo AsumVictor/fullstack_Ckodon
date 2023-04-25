8af39671a8c132e381aea47b64900da861a13778
AsumVictor <iamasum369@gmail.com>
2023-04-25 06:09:46 -0700

    webpack

--- Backend/webpack.config.js
+++ Backend/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/server.js',
   target: 'node',
   externals: [nodeExternals()],
   output: {
