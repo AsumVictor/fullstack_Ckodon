a22cf298c4cdf0709f528893cb765299db0ee5ae
Asum Victor <iamasum369@gmail.com>
2023-05-25 14:24:17 +0000

    Update apiSlice.js

--- mentors/src/app/api/apiSlice.js
+++ mentors/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: https://ckodon-api-backend.onrender.com',
+    baseUrl: 'https://ckodon-api-backend.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
