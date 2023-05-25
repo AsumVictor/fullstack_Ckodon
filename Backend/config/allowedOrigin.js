4a1c00684b8e036b249b4606caf9c22c592cac43
Asum Victor <iamasum369@gmail.com>
2023-05-25 14:23:01 +0000

    Update allowedOrigin.js

--- Backend/config/allowedOrigin.js
+++ Backend/config/allowedOrigin.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
        'https://admin-ckodon.onrender.com',
        'https://student-ckodon.onrender.com',
+       'https://mentor-ckodon.onrender.com',
        // 'http://localhost:5173',
        //'http://localhost:5174',
        //'http://localhost:5175',
