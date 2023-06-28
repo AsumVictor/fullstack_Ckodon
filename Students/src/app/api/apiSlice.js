3e10618d98680827d7e739155c6b5930225de53d
AsumVictor <iamasum369@gmail.com>
2023-06-28 08:02:13 +0000

    Comments

--- Students/src/app/api/apiSlice.js
+++ Students/src/app/api/apiSlice.js
@@ -15,9 +15,7 @@ const baseQuery = fetchBaseQuery({
 })
 
 const baseQueryWithReauth = async (args, api, extraOptions) => {
-    // console.log(args) // request url, method, body
-    // console.log(api) // signal, dispatch, getState()
-    // console.log(extraOptions) //custom like {shout: true}
+ 
 
     let result = await baseQuery(args, api, extraOptions)
 
